18b8fc9518e42bf3ff09d4f3e4caf938e0d9a05e
Greeshmithareddy <sanamgreeshmithareddy@gmail.com>
2025-12-30 20:48:25 +0530

    Task-3: Customized Mazer dashboard with JSON data integration

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,3 +6,17 @@ featherIcons.replace()
 // Mazer internal JS. Include this in your project to get
 // the sidebar running.
 import "./mazer" 
+fetch('/assets/data/data.json')
+  .then(res => res.json())
+  .then(data => {
+    const studentsEl = document.getElementById('totalStudents');
+    if (studentsEl) {
+      studentsEl.innerText = data.students;
+    }
+
+    const teachersEl = document.getElementById('totalTeachers');
+    if (teachersEl) {
+      teachersEl.innerText = data.teachers;
+    }
+  })
+  .catch(err => console.error('JSON load error:', err));
